faadfd83b854670e17691e92a116bbf5ff3cf1e4
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-06-22 01:17:59 -0300

    chore: integrating the new "AuthContext" in the "App" component and adding some comments to organize the imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+// Core
 import { BrowserRouter, Route } from 'react-router-dom';
+
+// Contexts
+import { AuthContextProvider } from './contexts/AuthContext';
 
 // Pages
 import { Home } from './pages/Home';
@@ -7,15 +11,17 @@ import { NewRoom } from './pages/NewRoom';
 const App = () => {
   return (
     <BrowserRouter>
-      <Route
-        exact
-        path="/"
-        component={Home}
-      />
-      <Route
-        path="/rooms/new"
-        component={NewRoom}
-      />
+      <AuthContextProvider>
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
+        <Route
+          path="/rooms/new"
+          component={NewRoom}
+        />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
