79486544aa02e64ca02f798b96f103ecd8b081f5
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-06-21 23:07:39 -0300

    chore: adding the react-router-dom lib and creating the initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Pages
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 const App = () => {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route
+        exact
+        path="/"
+        component={Home}
+      />
+      <Route
+        path="/rooms/new"
+        component={NewRoom}
+      />
+    </BrowserRouter>
   );
 }
 
